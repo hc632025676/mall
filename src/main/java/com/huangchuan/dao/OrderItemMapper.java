11e1d5fc583500356a44a3d9f95ab67aedfb4167
hc632025676 <hc632025676@qq.com>
2018-05-30 14:26:30 +0800

    project init commit

---
+++ src/main/java/com/huangchuan/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.huangchuan.dao;
+
+import com.huangchuan.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
